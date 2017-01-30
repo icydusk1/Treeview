e1500e54415351e5364f7005185b425a8ba784d7
Kim Jong Feel <DeveloperFEEL@gmail.com>
2017-01-30 22:54:50 +0900

    first example

--- TreeviewTest/TreeviewTest/MainWindow.xaml.cs
+++ TreeviewTest/TreeviewTest/MainWindow.xaml.cs
@@ -14,12 +14,47 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TreeviewTest {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window {
-		public MainWindow() {
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainWindowViewModel viewModel = new MainWindowViewModel();
+
+            MyTreeItem item1 = new MyTreeItem("North America");
+            MyTreeItem item11 = new MyTreeItem("USA");
+            MyTreeItem item111 = new MyTreeItem("ABC");
+            MyTreeItem item12 = new MyTreeItem("Canada");
+            MyTreeItem item13 = new MyTreeItem("Mexico");
+
+            item11.Children.Add(item111);
+
+            item1.Children.Add(item11);
+            item1.Children.Add(item12);
+            item1.Children.Add(item13);
+
+            viewModel.MyTreeItems.Add(item1);
+
+            MyTreeItem item2 = new MyTreeItem("Sorth America");
+            MyTreeItem item21 = new MyTreeItem("Argentina");
+            MyTreeItem item22 = new MyTreeItem("Brazil");
+            MyTreeItem item23 = new MyTreeItem("Uruguay");
+            
+            item2.Children.Add(item21);
+            item2.Children.Add(item22);
+            item2.Children.Add(item23);
+
+            viewModel.MyTreeItems.Add(item2);
+
+            DataContext = viewModel;
+        }
+    }
 }
